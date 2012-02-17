b1dcae2ceae5e8161798ac2bb1ec1b0e5d922529
Matt Malin <email@mattmalin.co.uk>
2012-02-17 22:39:45 +0000

    added feed_url

--- R/myepisodes.R
+++ R/myepisodes.R
@@ -6,3 +6,20 @@
 #' @aliases myepisodes package-myepisodes
 NULL
 
+feed_url <- function(uid, pwdmd5, feed = "mylist", onlyunacquired = TRUE, showignored = FALSE) {
+  # there are currently six feeds available
+  # mylist, today, yesterday, tomorrow, unacquired, unwatched, all
+  feed_url <- paste(
+    "http://www.myepisodes.com/rss.php?feed=",
+	feed,
+	"&onlyunacquired=",
+	ifelse(isTRUE(onlyunacquired), 1, 0),
+	"&showignored=",
+	ifelse(isTRUE(showignored), 1, 0),
+	"&sort=desc&uid=",
+	uid,
+	"&pwdmd5=",
+	pwdmd5,
+	sep = "")
+  return(feed_url)
+}
--- inst/tests/test_myepisodes.r
+++ inst/tests/test_myepisodes.r
@@ -1,1 +1,17 @@
 # myepisodes package tests
+library(testthat)
+
+test_that("feed urls are returned correctly", {
+  expect_that(
+    feed_url("foouser", "barpw", feed = "mylist", onlyunacquired = TRUE, showignored = FALSE),
+	is_identical_to("http://www.myepisodes.com/rss.php?feed=mylist&onlyunacquired=1&showignored=0&sort=desc&uid=foouser&pwdmd5=barpw")
+  )
+  expect_that(
+    feed_url("foouser", "barpw", feed = "given_feed", onlyunacquired = FALSE, showignored = FALSE),
+	is_identical_to("http://www.myepisodes.com/rss.php?feed=given_feed&onlyunacquired=0&showignored=0&sort=desc&uid=foouser&pwdmd5=barpw")
+  )
+  expect_that(
+    feed_url("foouser", "barpw", feed = "given_feed", onlyunacquired = TRUE, showignored = TRUE),
+	is_identical_to("http://www.myepisodes.com/rss.php?feed=given_feed&onlyunacquired=1&showignored=1&sort=desc&uid=foouser&pwdmd5=barpw")
+  )
+})
