8e42bd75acb0a61af5df4dfe175fc57d22c7722d
Matt Malin <email@mattmalin.co.uk>
2012-02-17 22:22:40 +0000

    package skeleton and description

---
+++ R/myepisodes.R
@@ -0,0 +1,8 @@
+#' myepisodes functions
+#'
+#' Various functions to play with MyEpisodes data
+#' @docType package
+#' @name myepisodes
+#' @aliases myepisodes package-myepisodes
+NULL
+
---
+++ inst/tests/test_myepisodes.r
@@ -0,0 +1,1 @@
+# myepisodes package tests
