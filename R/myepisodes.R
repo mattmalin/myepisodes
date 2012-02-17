bd167c8b23a452b167c0a39eb8adde015ead3115
Matt Malin <email@mattmalin.co.uk>
2012-02-17 22:53:11 +0000

    documentation for feed_url

--- R/myepisodes.R
+++ R/myepisodes.R
@@ -6,6 +6,21 @@
 #' @aliases myepisodes package-myepisodes
 NULL
 
+#' get the MyEpisodes RSS feed url for a desired feed
+#'
+#' Returns the custom MyEpisodes RSS feed url for a desired feed.
+#' @param uid myepisodes username
+#' @param pwdmd5 myepisodes MD5 hashed password
+#' @param feed the desired feed 
+#' currently there are: mylist, today, yesterday, tomorrow, unacquired, 
+#' unwatched, all.
+#' @param onlyunacquired TRUE/FALSE - You can use this to decide if you only 
+#' want the feed to list the episodes you haven't acquired.
+#' @param showignored TRUE/FALSE - You can use this to decide if you want the 
+#' feed to show your ignored shows too.
+#' @author Matt Malin <\email{R@@mattmalin.co.uk}>
+#' @examples feed_url("foouser", "321654321654321", feed = "mylist", onlyunacquired = TRUE, showignored = FALSE)
+#' @export
 feed_url <- function(uid, pwdmd5, feed = "mylist", onlyunacquired = TRUE, showignored = FALSE) {
   # there are currently six feeds available
   # mylist, today, yesterday, tomorrow, unacquired, unwatched, all
